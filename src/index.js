2740758f627f1970e3f9b5dbbbfa6e61b16bbdb7
vuflysocial <vuflysocial@gmaill.com>
2023-04-17 12:31:48 -0700

    icons

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "binance";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
