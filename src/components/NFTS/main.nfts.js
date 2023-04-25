a16ca69d45f7ade049c64463b44a4a4396c36b03
Mike McMath <100451372+vuflysocial@users.noreply.github.com>
2023-04-25 11:19:00 -0600

    Update main.nfts.js

--- src/components/NFTS/main.nfts.js
+++ src/components/NFTS/main.nfts.js
@@ -2,92 +2,84 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperclip } from "@fortawesome/free-solid-svg-icons";
 
+
 const nfts = () => {
-  const lists = [
-    {
-      question: "...",
-      li: false,
-      answer:
-        "",
-      color: "#00FFC2",
-    },
-    {
-      question: "Check back soon! maintenance is being done 🚧",
-      li: true,
-      answer: [
-        { list_answer: "..." },
-        {
-          list_answer:
-            "",
-        },
-      ],
-      color: "#FFFF00",
-    },
-    {
-      question: "",
-      li: false,
-      answer: "",
-      color: "#40E0D0",
-    },
-    {
-      question: "",
-      answer:
-        "",
-      color: "#00FF00",
-    },
-  ];
+
   return (
-    <div
-      style={{
-        backgroundImage:
-          'url("")',
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        opacity: 1,
-        height: "101vh",
-        width: "90vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <div className="p-2 mt-2">
-        {lists.map((list, index) => (
-          <div key={index} className="mb-5 mt-5">
-            <div
-              className="border-2 border-black px-3 py-2 rounded-lg"
-              style={{
-                boxShadow: "0.4rem 0.4rem 0 #222",
-                backgroundColor: list.color,
-              }}
-            >
-              <h1 className="text-black text-base md:text-2xl">
-                {list.question}
-              </h1>
-            </div>
-            <div className="mt-3 text-white-600 text-xs md:text-base">
-              {!list.li ? (
-                <>
-                  <span>{list.answer}</span>
-                </>
-              ) : (
-                <>
-                  <ul style={{ listStyleType: "circle" }}>
-                    {list.answer.map((ans, index) => (
-                      <li key={index}>{ans.list_answer}</li>
-                    ))}
-                  </ul>
-                </>
-              )}
-            </div>
-          </div>
-        ))}
-      </div>
+
+<div>
+<div>
+<div>
+
+<div>
+  <div>
+    <h3>Melos Favorite Artists</h3>
+  </div>
+  <div>
+<button href="/dashboard">
+  <div >Back To Dashboard</div>
+
+</button>
+  </div>
+
+  <br />
+<div>
+  <p>Haller.</p>
+</div>
+<iframe
+  width="560"
+  height="315"
+  src="https://www.youtube.com/embed/WHhemlO0mW4"
+  title="YouTube video player"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  allowFullScreen
+/>
+<div>
+
+</div>
+<br/>
+
+  <div>
+    <p>VIBE</p>
+  </div>
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/BIqeykm6l_Y"
+    title="YouTube video player"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen
+  />
+  <audio controls>
+    <source src="audio.mp3" type="audio/mpeg" />
+    Your browser does not support the audio element.
+  </audio>
+
+</div>
+<br />
+<div>
+  <p>DOTTY CA$H</p>
+</div>
+<iframe
+  width="560"
+  height="315"
+  src="https://www.youtube.com/embed/X3E_YmUNa90"
+  title="YouTube video player"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  allowFullScreen
+/>
+<div>
+
+</div>
+
+
+
+</div>
+</div>
     </div>
   );
 };
 
 export default nfts;
+
 
